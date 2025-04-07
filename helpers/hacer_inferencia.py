6525247821d23f7f4be7dfc5ab0a2a52150a7071
FERRAN PLANA CAMINERO <ferran.planacaminero@telefonica.com>
2025-04-07 21:11:09 +0200

    Added LiteLLM implementation

--- helpers/hacer_inferencia.py
+++ helpers/hacer_inferencia.py
@@ -2,7 +2,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import requests
 import json
-from groq import Groq
+from litellm import completion
 
 
 
@@ -17,98 +17,35 @@ def get_similar_chunks(question, chunks, embeddings, model, top_n):
 # Función para obtener la respuesta de un LLM
 def get_LLM_response(proveedor,apikey,endpoint_model,user_prompt,system_prompt):
 
-  if proveedor=="OpenRouter":
-    try:
-
-        response = requests.post(
-            url="https://openrouter.ai/api/v1/chat/completions",
-            headers={
-                "Authorization": f"Bearer {apikey}",
-                "Content-Type": "application/json"
-            },
-            data=json.dumps({
-                "model": endpoint_model,
-                "messages": [
-                {
-                    "role": "system",
-                    "content": system_prompt
-                },
-                {
-                    "role": "user",
-                    "content": user_prompt
-                }
-                ],
-                "temperature": 0,
-                "repetition_penalty": 0,
-            })
-            )
-
-        return response.json()['choices'][0]['message']['content']
-
-    except requests.exceptions.RequestException as e:
-      # Manejar errores de red o del servidor
-      print(f"Error de red o del servidor: {e}")
-      return "ERROR"
-
-    except (KeyError, IndexError) as e:
-      # Manejar errores de estructura de respuesta inesperada
-      print(f"Error en la estructura de la respuesta: {e}")
-      return "ERROR"
-
-    except json.JSONDecodeError as e:
-      # Manejar errores de decodificación JSON
-      print(f"Error al decodificar la respuesta JSON: {e}")
-      return "ERROR"
-
-    except Exception as e:
-      # Capturar cualquier otro error no previsto
-      print(f"Error inesperado: {e}")
-      return "ERROR"
-
-
-  elif proveedor=="Groq":
-    try:
-
-      client = Groq(
-        api_key=apikey,
+  try:
+    response = completion(
+      model=proveedor.lower()+"/"+endpoint_model,
+      messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt}
+      ],
+      api_key=apikey,
+      temperature=0,
+      presence_penalty=0,
       )
+    return response.choices[0].message.content
+  
+  except requests.exceptions.RequestException as e:
+    # Manejar errores de red o del servidor
+    print(f"Error de red o del servidor: {e}")
+    return "ERROR"
 
-      chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": system_prompt,
-            },
-            {
-                "role": "user",
-                "content": user_prompt,
-            }
-        ],
-        model=endpoint_model,
-        temperature=0,
-        frequency_penalty=0
-      )
+  except (KeyError, IndexError) as e:
+    # Manejar errores de estructura de respuesta inesperada
+    print(f"Error en la estructura de la respuesta: {e}")
+    return "ERROR"
 
-      return chat_completion.choices[0].message.content
+  except json.JSONDecodeError as e:
+    # Manejar errores de decodificación JSON
+    print(f"Error al decodificar la respuesta JSON: {e}")
+    return "ERROR"
 
-    except requests.exceptions.RequestException as e:
-      # Manejar errores de red o del servidor
-      print(f"Error de red o del servidor: {e}")
-      return "ERROR"
-
-    except (KeyError, IndexError) as e:
-      # Manejar errores de estructura de respuesta inesperada
-      print(f"Error en la estructura de la respuesta: {e}")
-      return "ERROR"
-
-    except json.JSONDecodeError as e:
-      # Manejar errores de decodificación JSON
-      print(f"Error al decodificar la respuesta JSON: {e}")
-      return "ERROR"
-
-    except Exception as e:
-      # Capturar cualquier otro error no previsto
-      print(f"Error inesperado: {e}")
-      return "ERROR"
-
-
+  except Exception as e:
+    # Capturar cualquier otro error no previsto
+    print(f"Error inesperado: {e}")
+    return "ERROR"
